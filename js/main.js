195b773bfc766fc6eb940c4c54d07d35c2ff3d0e
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2022-08-23 14:43:48 +0200

    Prems commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Fichier de base pour application web HTML/CSS/JS
+ * @author  Chloé Renaud
+ * @version 0.2
+ * @since   2019-08-22
+ */
+
+(function main() {
+  'use strict'; // Demande une interprétation stricte du code
+
+    let a = 'Bonjour';
+    let b = 'toto';
+
+    console.log(a, b);
+}()); // Main IIFE
+
